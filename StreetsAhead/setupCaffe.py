172ab8d7e25ccbfa4e23724794a3cac863011221
Matt George <mattgeorge1@gmail.com>
2014-06-12 21:17:17 -0700

    fix dir names in main and loop over objs

--- StreetsAhead/setupCaffe.py
+++ StreetsAhead/setupCaffe.py
@@ -26,7 +26,7 @@ def createLevelDB(imgDir, labelFile, outLevelDB):
 def computeImageMean(levelDB, outMeanProtoFile):
     """Port of make_imagenet_mean.sh"""
     toolDir = CAFFE_DIR + "/build/tools"
-    os.system("{}/compute_image_mean.bin {}/{} {}/{}".format(
+    os.system("GLOG_logtostderr=1 {}/compute_image_mean.bin {}/{} {}/{}".format(
         toolDir, LEVELDB_DIR, levelDB, PROTO_DIR, outMeanProtoFile))
 
 def train(prototxtFile):
@@ -37,21 +37,27 @@ def train(prototxtFile):
 
 if __name__ == "__main__":
 
-    trainDir = "/Users/mgeorge/insight/masterTrainDir"
-    valDir = "/Users/mgeorge/insight/masterValDir"
+    trainDir = "/Users/mgeorge/insight/masterTrainData"
+    valDir = "/Users/mgeorge/insight/masterValData"
 
-#    for name in ("Char0", "Char1", "Char2", "WordLen"):
-    for name in ("Char0",):
+    #    objectives = ("Char0", "Char1", "Char2", "WordLen")
+    objectives = ("Char0",)
 
-        trainLevelDB = "master_{}_train_leveldb_short".format(name)
-        valLevelDB = "master_{}_val_leveldb_short".format(name)
+    for obj in objectives:
+        print obj
+        print "creating leveldb"
+        trainLevelDB = "master_{}_train_leveldb_short".format(obj)
+        valLevelDB = "master_{}_val_leveldb_short".format(obj)
 
-        createLevelDB(trainDir, trainDir + "/train{}.txt".format(name),
+        createLevelDB(trainDir, trainDir + "/train{}.txt".format(obj),
                       trainLevelDB)
-        createLevelDB(valDir, valDir + "/val{}.txt".format(name), valLevelDB)
+        createLevelDB(valDir, valDir + "/val{}.txt".format(obj), valLevelDB)
 
-        trainMeanProto = "master_{}_mean_short.binaryproto".format(name)
+        print "computing image mean"
+        trainMeanProto = "master_{}_mean_short.binaryproto".format(obj)
         computeImageMean(trainLevelDB, trainMeanProto)
 
-        prototxtFile = "/Users/mgeorge/insight/protos/master_{}_solver_short.prototxt".format(name)
+        print "training"
+        prototxtFile = "/Users/mgeorge/insight/protos/" \
+            "master_{}_solver_short.prototxt".format(obj)
         train(prototxtFile)
