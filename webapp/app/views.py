632d15f5b75f39456577a23e9fb10532cbc85cec
Matt George <mattgeorge1@gmail.com>
2014-06-22 16:05:09 -0700

    fix typo in views.py

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -10,13 +10,13 @@ import pymysql
 
 try:
     from StreetsAhead import ingest, imToText
-    from StreetsAhead.camfind import *
+    from StreetsAhead.config import *
 except ImportError: # add parent dir to python search path
     import os, sys
     path, filename = os.path.split(__file__)
     sys.path.append(os.path.abspath(os.path.join(path,"../../../StreetsAhead")))
     from StreetsAhead import ingest, imToText
-    from StreetsAhead.camfind import *
+    from StreetsAhead.config import *
 
 with open(MYSQL_KEY_FILE, 'r') as ff:
     username, password = ff.readline().split()
